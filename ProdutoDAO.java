439b4de02b7ef7bb0f83aa78e7bdd3dafa056c27
ByakuranN7 <51470781+ByakuranN7@users.noreply.github.com>
2026-07-21 16:58:17 -0300

    Create ProdutoDAO.java

---
+++ ProdutoDAO.java
@@ -0,0 +1,30 @@
+import java.*;
+
+public class ProdutoDAO{
+
+    public void inserir(Produto produto){
+
+    }
+
+    public void atualizar(Produto produto){
+
+    }
+
+    public void excluir(int id){
+
+    }
+
+    public void excluirTodos(){
+
+    }
+
+    public void consultarPorId(int id){
+
+    }
+
+    public void listarTodos(){
+        
+    }
+
+
+}
