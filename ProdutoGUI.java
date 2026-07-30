8ec589144ab709f4b2a26d003135fcff433d990a
ByakuranN7 <51470781+ByakuranN7@users.noreply.github.com>
2026-07-30 18:31:01 -0300

    feat: adiciona formulário de cadastro de produtos
    - adiciona campos para nome, quantidade e preço
    - adiciona ComboBox para seleção do status
    - organiza os componentes em HBox

--- ProdutoGUI.java
+++ ProdutoGUI.java
@@ -28,5 +28,36 @@ public class ProdutoGUI extends Application{
         conexaoDB = ConexaoDB.conectar();
         produtoDAO = new ProdutoDAO(conexaoDB); //inicializa o objeto
         todosProdutos = FXCollections.observableArrayList(produtoDAO.listarTodos());
+
+        palco.setTitle("Gerenciamento de Estoque de Produtos");
+
+        Vbox vbox = new VBox();
+        vbox.setPadding(new Insets(10, 10, 10, 10));
+        vbox.setSpacing(10);
+
+        HBox nomeProdutosBox = new HBox();
+        nomeProdutoBox.setSpacing(10);
+        Label nomeLabel = new Label("Produto:");
+        nomeInput = new TextField();
+        nomeProdutoBox.getChildren().addAll(nomeLabel, nomeInput);
+
+        HBox quantidadeBox = new HBox();
+        quantidadeBox.setSpacing(10);
+        Label quantidadeLabel = new Label("Quantidade:");
+        quantidadeInput = new TextField();
+        quantidadeBox.getChildren().addAll(quantidadeLabel, quantidadeInput);
+
+        HBox precoBox = new HBox();
+        precoBox.setSpacing(10);
+        Label precoLabel = new Label("Preço:");
+        precoInput = new TextField();
+        precoBox.getChildren().addAll(precoLabel, precoInput);
+
+        HBox statusBox = new HBox();
+        statusBox.setSpacing(10);
+        Label statusLabel = new Label("Status:");
+        statusComboBox = new ComboBox<>();
+        statusComboBox.getItems().addAll("Estoque Normal", "Estoque Baixo");
+        statusBox.getChildren().addAll(statusLabel, statusComboBox);
     }
 }
